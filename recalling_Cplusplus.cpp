271f51a57c51b94c883fe9ff443859d3b8ee04d3
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-01-05 16:01:21 +0500

    initializing Repo

---
+++ recalling_Cplusplus.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    cout << "recalling c++ language yeeyyyyyyyyy=234324244" << endl;
+    string name;
+    cout << "Enter your name : " << endl;
+    cin >> name;
+    cout << name << " Welcome to the DSA" << endl; 
+
+    return 0;
+
+
+}
