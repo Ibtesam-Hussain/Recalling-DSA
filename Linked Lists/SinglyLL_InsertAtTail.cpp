9a2d130a158846fd79868229f5629c7d767f90e8
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-03-09 17:24:22 +0500

    PRINT_NODE.H HEADER CREATED TO AVOID PRINTING LL AGAIN AND AGAIN

---
+++ Linked Lists/SinglyLL_InsertAtTail.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdlib.h>
+#include "redundantFuncs/print_node.h"
+
+using namespace std;
+
+
+// class Node {
+//     public:
+//     int data;
+//     Node* next;
+
+//     Node (int data){ //deflt constrtr
+//         this->data = data;
+//         this->next = NULL;
+//     }
+
+// };
+
+
+void insertionAtTail(Node* &head, int data) {
+    //PREVIOUS LOGIC:
+    //new node create 
+    // Node* newNode = new Node(data);
+
+    // //replace curr node next pointer from NULL to newNode
+    // newNode = head->next;      // THIS overwrites the pointer
+
+    // //newNode ka jo next pointer ha, point that to forward NULL
+    // newNode->next = NULL;     // ➜ dereferencing NULL when head->next is NULL
+
+
+    Node* newNode = new Node(data);
+
+    if (head == nullptr) {          // empty list case
+        head = newNode;
+        return;
+    }
+
+    Node* curr = head;
+    while (curr->next != nullptr)    // walk to the last node
+        curr = curr->next;
+
+    curr->next = newNode;            // attach the new node
+}
+
+int main(int argc, char const *argv[])
+{
+    Node* head = NULL;
+    
+    insertionAtTail(head, 101);
+    printLL(head);
+    insertionAtTail(head, 102);
+    printLL(head);
+    insertionAtTail(head, 103);
+    printLL(head);
+    insertionAtTail(head, 104);
+    printLL(head);
+    insertionAtTail(head, 105);
+    printLL(head);
+    insertionAtTail(head, 106);
+    printLL(head);
+    insertionAtTail(head, 107);
+    printLL(head);
+    return 0;
+}
---
+++ Linked Lists/redundantFuncs/print_node.h
@@ -0,0 +1,35 @@
+
+//UPDATE: I MAKE THIS HEADER FILE BCZ I DONT WANT TO WRITE printLL() FUNC IN EACH FILE 
+// AVOIDING THE REDUNDANCY
+
+#ifndef PRINT_NODE_H
+#define PRINT_NODE_H
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+//using inline to avoid multiple definitions error in future .cpp files
+ 
+inline void printLL(Node* &head){
+    Node* curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " -> ";
+        curr = curr->next; 
+    }
+    cout << endl;
+}
+
+
+#endif
