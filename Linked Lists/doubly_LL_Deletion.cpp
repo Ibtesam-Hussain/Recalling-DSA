37e370083244ece95b22ece9021e8b4e5c05852b
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-03-15 16:30:29 +0500

    ADDED: Deletion at head

---
+++ Linked Lists/doubly_LL_Deletion.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+#include "redundantFuncs/print_doubly_nodes.h"
+
+using namespace std;
+
+void insertAtTail(Node* &tail, Node* &head, int data){
+    Node* lastNode = tail;
+    Node*  newNode = new Node(data);
+
+    // if there's single node its both head/tail but the logic for inserting at would be same
+    if(tail == NULL){  //tail or head whatever 
+        tail = newNode;
+        head = newNode;
+        return;
+    }
+
+
+    lastNode->next = newNode;
+    newNode->prev = lastNode;
+    //change label of tail from lastNode to newNode as newNode will now our tail
+    tail = newNode;
+    // or wese bhi newNode ka next NULL krny ki need nhi coz wo phele hi class constructor mei defined ha
+
+}
+
+void deleteAtHead(Node* &head){
+    Node* curr = head;
+    
+    curr->next->prev = NULL; //break the  of agay wala node
+    head = curr->next; //move the head label to agay wala node
+    curr->next = NULL; // break the link of current head node
+    delete curr; //memory free of current head 
+}
+
+
+int main(int argc, char const *argv[])
+{
+    Node* head = new Node(100);
+    Node* tail = head;
+
+    insertAtTail(tail, head, 101);
+    printDLL(head);
+    insertAtTail(tail, head, 102);
+    printDLL(head);
+    insertAtTail(tail, head, 103);
+    printDLL(head);
+    insertAtTail(tail, head, 104);
+    printDLL(head);
+    insertAtTail(tail, head, 105);
+    printDLL(head);
+
+    cout << "Delete at head : " << endl;
+    deleteAtHead(head);
+    printDLL(head);
+    cout << "Delete at head (2): " << endl;
+    deleteAtHead(head);
+    printDLL(head);
+    return 0;
+}
