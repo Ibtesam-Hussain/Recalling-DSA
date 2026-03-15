8ebd80aa10670e997f4961e7a862328d126f9021
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-03-15 23:34:52 +0500

    added circular_LL insert @ middle in between position

--- Linked Lists/CircularLL_Insertion.cpp
+++ Linked Lists/CircularLL_Insertion.cpp
@@ -39,6 +39,38 @@ void insertAtTail(Node* &tail, int data)
     newNode->next = tail;
 }
 
+
+void insertAtMiddle(Node* &head, int pos, int data){
+    Node* curr = head;
+    Node* newNode = new Node(data);
+
+    int len = 1;
+    
+    while (curr->next != head)
+    {
+        curr = curr->next;
+        len++; 
+    }
+
+    if(pos == 1 || pos == len+1){ //tail considering
+        cout << "This func is for in between positions not for edge cases" << endl;
+    }
+
+    curr = head; //reset curr ptr to new location
+    // SIMPLE JUST LIKE SINGLY LL
+    int count = 1;
+    while (count < pos-1)
+    {
+        curr = curr->next;
+        count++;
+    }
+
+    newNode->next = curr->next;
+    curr->next = newNode;
+
+    
+}
+
 int main(int argc, char const *argv[])
 {
     Node* head = NULL;
@@ -56,5 +88,25 @@ int main(int argc, char const *argv[])
     printCLL(head);
     insertAtTail(head, 23);
     printCLL(head);
+
+    insertAtMiddle(head, 3, 19);
+    printCLL(head);
     return 0;
 }
+
+
+//OUPUT:
+// 10 -> (back to HEAD)
+// 20 -> 10 -> (back to HEAD)
+// 30 -> 20 -> 10 -> (back to HEAD)
+// 30 -> 20 -> 10 -> 18 -> (back to HEAD)
+// 30 -> 20 -> 10 -> 18 -> 11 -> (back to HEAD)      
+// 30 -> 20 -> 10 -> 18 -> 11 -> 23 -> (back to HEAD)
+
+// 10 -> (back to HEAD)
+// 20 -> 10 -> (back to HEAD)
+// 30 -> 20 -> 10 -> (back to HEAD)
+// 30 -> 20 -> 10 -> 18 -> (back to HEAD)
+// 30 -> 20 -> 10 -> 18 -> 11 -> (back to HEAD)      
+// 30 -> 20 -> 10 -> 18 -> 11 -> 23 -> (back to HEAD)
+// 30 -> 20 -> 19 -> 10 -> 18 -> 11 -> 23 -> (back to HEAD)  added 19 to 3rd location
