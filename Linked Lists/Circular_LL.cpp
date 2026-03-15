610b45c91f69d39fdf65589381c527fbcd7193e2
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-03-15 17:31:01 +0500

    Started Circular LL (CLL structure and printing)

--- Linked Lists/Circular_LL.cpp
+++ Linked Lists/Circular_LL.cpp
@@ -12,11 +12,20 @@ int main(int argc, char const *argv[])
     Node* node1 = new Node(12);
     Node* node2 = new Node(13);
     Node* node3 = new Node(14);
+    Node* node4 = new Node(15);
+    Node* node5 = new Node(16);
+    Node* node6 = new Node(17);
+    Node* node7 = new Node(18);
+
     
 
     node1->next = node2;
     node2->next = node3;
-    node3->next = node1; //making it in a loop (circular)
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    node6->next = node7;
+    node7->next = node1; //making it in a loop (circular) 
 
     printCLL(node1);
 
