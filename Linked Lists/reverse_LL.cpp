c1f186436e8caebd0a3bbd94a273bf17438e4c51
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-03-20 15:30:01 +0500

    Added reverseLL approach v1

---
+++ Linked Lists/reverse_LL.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdlib.h>
+#include "redundantFuncs/print_node.h"
+
+using namespace std;
+
+
+// T.C IS O(N) AND S.C IS O(1)
+void reverseLL(Node* &head){
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+    // looping till curr is NULL
+    while (curr != NULL)
+    {
+        // maintaining a forward link so we dont loose track of the remaining nodes of the LL
+        forward = curr->next;
+
+        //iterating to other nodes and setting the curr, prev and forward at their appropriate positions
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+
+    }
+    head = prev; //lastly declare the prev as head bcz forward and curr now pointing at end (NULL)
+    
+}
+
+int main(int argc, char const *argv[])
+{
+
+    //insertion b4 reverse
+    Node* head = new Node(101);
+    head->next = new Node(102);
+    head->next->next = new Node(103);
+    head->next->next->next = new Node(104);
+
+    cout << "Original LL : " << endl;
+    printLL(head);
+    cout << "Reversed LL : " << endl;
+    reverseLL(head);
+    printLL(head);
+    return 0;
+}
+
+
+// Original LL :
+// 101 -> 102 -> 103 -> 104 ->
+// Reversed LL :
+// 104 -> 103 -> 102 -> 101 ->
