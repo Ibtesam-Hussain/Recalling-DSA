f30ecd11b50daf3c343d089153b7944179eb3b1c
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-06-27 16:59:42 +0500

    ADDED: binary-search-optimize.py

---
+++ binary-search-optimize.py
@@ -0,0 +1,28 @@
+def binarySearch(arr, target):
+    start = 0
+    end = len(arr) - 1
+    # print(start, end)
+
+    while start <= end:
+        #MAX INT ISSUE
+        # mid = (start + end)//2
+        mid = start + (end - start)//2
+
+        #target present at middle
+        if target == arr[mid]:
+            return mid
+
+        if target < arr[mid]:
+            end = mid - 1
+
+        if  target > arr[mid]:
+            start = mid + 1
+
+    
+    return -1  
+
+
+
+arr = [-1, 0, 3, 4, 5, 9, 12]
+target = 12
+print(f'target found at index {binarySearch(arr, target)}')
